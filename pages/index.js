f07e4cf995f225262b49848b4f9c3b1779f6d9ca
kodcanlisi <developerindiee@gmail.com>
2021-12-13 22:36:35 +0300

    Video section

--- pages/index.js
+++ pages/index.js
@@ -183,6 +183,16 @@ const About = () => {
                     </a>
                 </div>
 
+                <div className="m-2">
+                    <a
+                        href="#"
+                        target="_blank"
+                        class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+                    >
+                        <span>Sunum Videosu(Eklenecek)</span>
+                    </a>
+                </div>
+
                 <div>
 
                 </div>
