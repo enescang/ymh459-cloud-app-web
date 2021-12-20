924d6635beb4aefb7c6022fe53ef48ed1c140f42
kodcanlisi <developerindiee@gmail.com>
2021-12-20 23:07:05 +0300

    Authentication pages: login

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  axios.defaults.baseURL = "http://localhost:4000";
+  
+  useEffect(()=>{
+    const token = localStorage.getItem('friday_token');
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  }, []);
   return <Component {...pageProps} />
 }
 
